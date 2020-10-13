7d314f200bed9073c99a2a024d3d9576d74cb8eb
Craig Smith <craiglet@gmail.com>
2020-10-13 11:15:51 -0400

    change where and where_tag to field and field_tag

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -21,7 +21,7 @@ class ModelForTest(Model):
 class TestOpNode:
     def test_find_in_parents(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        q = db.query(ModelForTest).where('intcol').eq(50)
+        q = db.query(ModelForTest).field('intcol').eq(50)
         assert q.find_in_parents('model') == (True, ModelForTest)
 
     def test_find_columns(self, tmpdir):
@@ -36,34 +36,34 @@ class TestOpNode:
 class TestListResultOpNode:
     def test_eq(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        eq = db.query(ModelForTest).where('intcol').eq(50)
+        eq = db.query(ModelForTest).field('intcol').eq(50)
         assert eq.perform_comparison(eq.comparison_value, 50) == True
         assert eq.perform_comparison(eq.comparison_value, 49) == False
 
     def test_lt(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        lt = db.query(ModelForTest).where('intcol').lt(50)
+        lt = db.query(ModelForTest).field('intcol').lt(50)
         assert lt.perform_comparison(lt.comparison_value, 51) == True
         assert lt.perform_comparison(lt.comparison_value, 50) == False
         assert lt.perform_comparison(lt.comparison_value, 49) == False
 
     def test_lte(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        lte = db.query(ModelForTest).where('intcol').lte(50)
+        lte = db.query(ModelForTest).field('intcol').lte(50)
         assert lte.perform_comparison(lte.comparison_value, 51) == True
         assert lte.perform_comparison(lte.comparison_value, 50) == True
         assert lte.perform_comparison(lte.comparison_value, 49) == False
 
     def test_gt(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        gt = db.query(ModelForTest).where('intcol').gt(50)
+        gt = db.query(ModelForTest).field('intcol').gt(50)
         assert gt.perform_comparison(gt.comparison_value, 49) == True
         assert gt.perform_comparison(gt.comparison_value, 50) == False
         assert gt.perform_comparison(gt.comparison_value, 51) == False
 
     def test_gte(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        gte = db.query(ModelForTest).where('intcol').gte(50)
+        gte = db.query(ModelForTest).field('intcol').gte(50)
         assert gte.perform_comparison(gte.comparison_value, 49) == True
         assert gte.perform_comparison(gte.comparison_value, 50) == True
         assert gte.perform_comparison(gte.comparison_value, 51) == False
@@ -72,24 +72,24 @@ class TestListResultOpNode:
 class TestComparisonOp:
     def test_find_where_field(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        eq = db.query(ModelForTest).where('intcol').eq(50)
+        eq = db.query(ModelForTest).field('intcol').eq(50)
         assert eq.find_where_fields() == ['intcol']
 
-        eq = db.query(ModelForTest).where_tag('t1').eq(50)
+        eq = db.query(ModelForTest).field_tag('t1').eq(50)
         assert eq.find_where_fields() == ['intcol']
 
-        eq = db.query(ModelForTest).where_tag('t2').eq(50)
+        eq = db.query(ModelForTest).field_tag('t2').eq(50)
         assert eq.find_where_fields() == ['intcol', 'floatcol']
 
-        eq = db.query(ModelForTest).where_tag('t3').eq(50)
+        eq = db.query(ModelForTest).field_tag('t3').eq(50)
         assert eq.find_where_fields() == ['floatcol']
 
-        eq = db.query(ModelForTest).where_tag('t4').eq(50)
+        eq = db.query(ModelForTest).field_tag('t4').eq(50)
         assert eq.find_where_fields() == ['stringcol']
 
     def test_filter_list(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        eq = db.query(ModelForTest).where('intcol').eq(50)
+        eq = db.query(ModelForTest).field('intcol').eq(50)
 
     def test_eval(self, tmpdir):
         f1 = tmpdir.join("f1.csv")
@@ -102,7 +102,7 @@ class TestComparisonOp:
 100,11111.1,foobar4,2020-08-05 17:44:49.732176
 """)
         db = CsvDatabase(str(tmpdir))
-        results = db.query(ModelForTest).where('intcol').eq(50).eval()
+        results = db.query(ModelForTest).field('intcol').eq(50).eval()
         assert len(results) == 2
         assert results[0].intcol == 50
         assert results[1].intcol == 50
@@ -115,14 +115,14 @@ class TestComparisonOp:
 class TestWhereOp:
     def test_where_op(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        where = db.query(ModelForTest).where('stringcol')
+        where = db.query(ModelForTest).field('stringcol')
         assert where.where_fields == ['stringcol']
 
 
 class TestWhereTagOp:
     def test_where_tag_op(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
-        where = db.query(ModelForTest).where_tag('t2')
+        where = db.query(ModelForTest).field_tag('t2')
         assert len(where.where_fields) == 2
         assert 'intcol' in where.where_fields
         assert 'floatcol' in where.where_fields
@@ -145,11 +145,11 @@ class TestQueryOp:
     def test_where(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
         with pytest.raises(AssertionError) as error:
-            where = db.query(ModelForTest).where('badcol')
+            where = db.query(ModelForTest).field('badcol')
         assert "model 'ModelForTest' does not have field 'badcol'" == str(error.value)
 
     def test_where_tag(self, tmpdir):
         db = CsvDatabase(str(tmpdir))
         with pytest.raises(AssertionError) as error:
-            where = db.query(ModelForTest).where_tag('badtag')
+            where = db.query(ModelForTest).field_tag('badtag')
         assert "model 'ModelForTest' does not have tag 'badtag'" == str(error.value)
